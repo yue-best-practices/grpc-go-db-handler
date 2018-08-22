76e9f7b9b60108d25aad3c977a5b1bad951be4df
Yuanjianxin <1814888800@qq.com>
2018-08-22 14:07:16 +0800

    添加环境变量POOL_SIZE 设置

--- conf.go
+++ conf.go
@@ -3,11 +3,13 @@ package grpc_go_db_handler
 import (
 	"os"
 	"fmt"
+	"strconv"
 )
 
 var (
 	address=""
 	source=""
+	poolSize=5
 )
 
 
@@ -23,6 +25,19 @@ func checkEnv() error{
 
 	if sr!=""{
 		source=sr
+	}
+
+	pSize:=os.Getenv("POOL_SIZE")
+	if pSize!=""{
+
+		ps,err:=strconv.Atoi(pSize)
+
+		if err!=nil{
+			return fmt.Errorf("env POOL_SIZE is invalid")
+		}
+
+		poolSize=ps
+
 	}
 
 	return nil
